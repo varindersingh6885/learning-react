6cc6739b3191110d092c5e9e3b372e3f363ee860
Varinder Singh <varindersingh6885@gmail.com>
2023-08-15 17:08:55 +0530

    episode 03 setup

---
+++ 03. Laying the foundation/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const parent = React.createElement(
+  "div",
+  {
+    id: "parent-div",
+  },
+  React.createElement("div", { id: "child-div" }, [
+    React.createElement("h2", { id: "heading" }, "Hello World from React! 1"),
+    React.createElement("h2", { id: "heading" }, "Hello World from React! 2"),
+  ])
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+console.log(parent);
+root.render(parent);
