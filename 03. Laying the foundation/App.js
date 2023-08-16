4ee70b242b74a95a4d09853365fe9e4253f03a1f
Varinder Singh <varindersingh6885@gmail.com>
2023-08-16 13:39:37 +0530

    react functional component

--- 03. Laying the foundation/App.js
+++ 03. Laying the foundation/App.js
@@ -1,15 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement("h1", { id: "heading" }, "Hello World");
+// React Element
+const heading = <h1 id="heading">Hello World using JSX</h1>;
 
-// JSX is transpiled before it reaches the JS Engine by Parcel
-// Parcel is using Babel to transpile the JSX
-
-// JSX => React.createElement => JS Object => HTMLElment(render)
-const jsxHeading = <h1 id="heading">Hello World using JSX</h1>;
+// React Functional Component
+const HeadingComponent = () => (
+  <div id="container">
+    <h1 id="heading">h1 in functional component</h1>;
+  </div>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-console.log(root);
-root.render(jsxHeading);
+root.render(heading);
