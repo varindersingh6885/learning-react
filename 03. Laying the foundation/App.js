865ce59c5037307893efe27232ebe5c4437ae742
Varinder Singh <varindersingh6885@gmail.com>
2023-08-16 12:46:19 +0530

    add comments regarding how JSX is transpiled

--- 03. Laying the foundation/App.js
+++ 03. Laying the foundation/App.js
@@ -3,6 +3,10 @@ import ReactDOM from "react-dom/client";
 
 const heading = React.createElement("h1", { id: "heading" }, "Hello World");
 
+// JSX is transpiled before it reaches the JS Engine by Parcel
+// Parcel is using Babel to transpile the JSX
+
+// JSX => React.createElement => JS Object => HTMLElment(render)
 const jsxHeading = <h1 id="heading">Hello World using JSX</h1>;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
