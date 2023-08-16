180563f6c20c0757c9fccbe14bc9d6525ccd0162
Varinder Singh <varindersingh6885@gmail.com>
2023-08-16 14:45:10 +0530

    using React Element inside JSX

--- 03. Laying the foundation/App.js
+++ 03. Laying the foundation/App.js
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// React Functional Component
-const Title = () => <h1>this is a title</h1>;
+// React Element
+const title = <h1>this is a title</h1>;
 
 // React Functional Component
 const HeadingComponent = () => (
   <div id="container">
-    <Title />
+    {title}
     <h1 id="heading">20 + 10 = {20 + 10}</h1>
   </div>
 );
