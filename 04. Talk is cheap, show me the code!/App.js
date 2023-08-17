656ae744b4350add60c84814712189f43530babf
Varinder Singh <varindersingh6885@gmail.com>
2023-08-17 10:42:00 +0530

    create componenets -> AppLayout, Header, Body, RestaurantCard

--- 04. Talk is cheap, show me the code!/App.js
+++ 04. Talk is cheap, show me the code!/App.js
@@ -10,6 +10,8 @@ import ReactDOM from "react-dom/client";
  *  - Search
  *  - RestaurantContainer
  *    - RestaurantCard
+ *      - Img
+ *      - Name of restaurant, star rating, cuisine
  * Footer
  *  - Copyright disclaimer
  *  - Links
@@ -17,6 +19,55 @@ import ReactDOM from "react-dom/client";
  *  - Contract
  */
 
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png?nwm=1&nws=1&industry=food&sf="
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+const RestaurantCard = () => {
+  return (
+    <div className="restaurant-card">
+      <h3>Brothers Dhabha</h3>
+    </div>
+  );
+};
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="restaurant-container">
+        <RestaurantCard />
+      </div>
+    </div>
+  );
+};
+
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  );
+};
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<HeadingComponent />);
+root.render(<AppLayout />);
