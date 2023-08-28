f5f48e935343a331601be93920abce6c888c9974
Varinder Singh <varindersingh6885@gmail.com>
2023-08-28 23:08:58 +0530

    fetch data from api

--- 06. Exploring the world/src/components/Body.js
+++ 06. Exploring the world/src/components/Body.js
@@ -1,8 +1,19 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import RestaurantCard from "./RestaurantCard";
 import { restaurants } from "../utils/mockData";
 const Body = () => {
   const [listOfRestaurants, setListOfRestaurants] = useState(restaurants);
+
+  const fetchRestaurantsData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+    console.log(data);
+  };
+
+  useEffect(() => {
+    fetchRestaurantsData();
+  }, []);
 
   const filterTopRated = () => {
     return listOfRestaurants.filter(
