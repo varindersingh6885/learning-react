5a0048677603d055fc3e9153cbc7490f59abec5b
Varinder Singh <varindersingh6885@gmail.com>
2023-09-06 21:06:15 +0530

    episode 08 setup

---
+++ 08. Let's Get Classy/src/components/About.js
@@ -0,0 +1,10 @@
+const About = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <h2>This is food app built in Namaste React Web Series</h2>
+    </div>
+  );
+};
+
+export default About;
---
+++ 08. Let's Get Classy/src/components/Header.js
@@ -0,0 +1,47 @@
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { LOGO_URL } from "../utils/constants";
+
+const Header = () => {
+  const [btnName, setBtnName] = useState("Login");
+
+  const changeBtnName = () => {
+    setBtnName((prevValue) => (prevValue === "Login" ? "Logout" : "Login"));
+  };
+
+  // if no dependency array then useEffect is called on every render
+  // if dependency array is empty = [] then useEffect is just called once on initial render
+  // if dependency array is [btnName] then useEffect is called everytime btnName is updated
+  useEffect(() => {
+    console.log("useEffect called");
+  }, [btnName]);
+
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img className="logo" src={LOGO_URL} alt="logo" />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About Us</Link>
+          </li>
+          <li>
+            <Link to="contact">Contact Us</Link>
+          </li>
+          <li>Cart</li>
+          <li>
+            <button onClick={changeBtnName} className="log-btn">
+              {btnName}
+            </button>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
