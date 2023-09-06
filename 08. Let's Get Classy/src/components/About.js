da2ee804b9aa690ae744080d0283f0c2abf2eacb
Varinder Singh <varindersingh6885@gmail.com>
2023-09-06 22:13:12 +0530

    create simple class based component

--- 08. Let's Get Classy/src/components/About.js
+++ 08. Let's Get Classy/src/components/About.js
@@ -1,8 +1,19 @@
+import User from "./User";
+import UserClass from "./UserClass";
+
 const About = () => {
   return (
     <div>
-      <h1>About</h1>
-      <h2>This is food app built in Namaste React Web Series</h2>
+      <div className="m-1">
+        <h1>About</h1>
+        <h2>This is food app built in Namaste React Web Series</h2>
+      </div>
+
+      <div className="m-1">
+        <h3>Team members:</h3>
+        <User />
+        <UserClass />
+      </div>
     </div>
   );
 };
---
+++ 08. Let's Get Classy/src/components/User.js
@@ -0,0 +1,16 @@
+const User = () => {
+  return (
+    <div className="user-card">
+      <h2>Name: Varinder Singh</h2>
+      <h3>Location: Punjab</h3>
+      <h4>
+        Contact:{" "}
+        <a href="mailto:varindersingh6885@gmail.com">
+          varindersingh6885@gmail.com
+        </a>
+      </h4>
+    </div>
+  );
+};
+
+export default User;
