140053167f50bf5c8f365704947865f6f3cab240
Varinder Singh <varindersingh6885@gmail.com>
2023-10-11 21:35:29 +0530

    learn query methods and how to assert elements

---
+++ 13. Time for test/src/__tests__/contact.test.js
@@ -0,0 +1,29 @@
+import { render, screen } from "@testing-library/react";
+import Contact from "../components/Contact";
+import "@testing-library/jest-dom";
+
+test("Should render Contact Us page", () => {
+  render(<Contact />);
+
+  const heading = screen.getByRole("heading");
+
+  expect(heading).toBeInTheDocument();
+});
+
+test("Should render Email input field", () => {
+  render(<Contact />);
+
+  const emailInput = screen.getByPlaceholderText("Email Id");
+
+  expect(emailInput).toBeInTheDocument();
+});
+
+test("Should render Submit button", () => {
+  render(<Contact />);
+
+  const submitButton = screen.getByRole("button", {
+    name: "Submit",
+  });
+
+  expect(submitButton).toBeInTheDocument();
+});
