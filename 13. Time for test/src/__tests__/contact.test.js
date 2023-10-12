54f49cba653a417f209e8d3a3b0a8e7b1535661a
Varinder Singh <varindersingh6885@gmail.com>
2023-10-12 22:36:49 +0530

    test Header component

--- 13. Time for test/src/__tests__/contact.test.js
+++ 13. Time for test/src/__tests__/contact.test.js
@@ -2,36 +2,38 @@ import { render, screen } from "@testing-library/react";
 import Contact from "../components/Contact";
 import "@testing-library/jest-dom";
 
-test("Should render Contact Us page", () => {
-  render(<Contact />);
+describe("Contact Us page testcases", () => {
+  test("Should render Contact Us page", () => {
+    render(<Contact />);
 
-  const heading = screen.getByRole("heading");
+    const heading = screen.getByRole("heading");
 
-  expect(heading).toBeInTheDocument();
-});
-
-test("Should render Email input field", () => {
-  render(<Contact />);
-
-  const emailInput = screen.getByPlaceholderText("Email Id");
-
-  expect(emailInput).toBeInTheDocument();
-});
-
-test("Should render Submit button", () => {
-  render(<Contact />);
-
-  const submitButton = screen.getByRole("button", {
-    name: "Submit",
+    expect(heading).toBeInTheDocument();
   });
 
-  expect(submitButton).toBeInTheDocument();
-});
+  test("Should render Email input field", () => {
+    render(<Contact />);
 
-test("Should render 2 text inputs", () => {
-  render(<Contact />);
+    const emailInput = screen.getByPlaceholderText("Email Id");
 
-  const textInputs = screen.getAllByRole("textbox");
+    expect(emailInput).toBeInTheDocument();
+  });
 
-  expect(textInputs.length).toBe(2);
+  test("Should render Submit button", () => {
+    render(<Contact />);
+
+    const submitButton = screen.getByRole("button", {
+      name: "Submit",
+    });
+
+    expect(submitButton).toBeInTheDocument();
+  });
+
+  test("Should render 2 text inputs", () => {
+    render(<Contact />);
+
+    const textInputs = screen.getAllByRole("textbox");
+
+    expect(textInputs.length).toBe(2);
+  });
 });
